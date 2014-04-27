46913ef93360400b9d0cfdcec5735cfbfa9abd78
Jack Daines <dainesj@gmail.com>
2014-04-26 19:34:03 -0700

    change sensitivity

--- touchpad-config.c
+++ touchpad-config.c
@@ -90,13 +90,13 @@ int main( int argc, char *argv[] )
                     else if(diff_x > 85 && prev_fingers ==3){
                         popen("xdotool key alt+Right", "re");
                     }
-                    else if(diff_y > 500 && prev_fingers ==3){
+                    else if(diff_y > 400 && prev_fingers ==3){
                         popen("xdotool key super+t", "re");
                     }
-                    else if(diff_y > -500 && prev_fingers ==4){
+                    else if(diff_y > -400 && prev_fingers ==4){
                         popen("xdotool key ctrl+alt+d", "re");
                     }
-                    else if(diff_y > 500 && prev_fingers ==4){
+                    else if(diff_y > 400 && prev_fingers ==4){
                         popen("xdotool key ctrl+alt+d", "re");
                     }
 
